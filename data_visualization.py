0c1503fb5c90f6df935cb2880b29178735d7aa9a
evenaage <evenaage.smedshaug@gmail.com>
2022-10-09 17:31:47 +0200

    minor fixes

--- data_visualization.py
+++ data_visualization.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import geopandas as gpd
+#import geopandas as gpd
 import re
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
@@ -9,12 +9,13 @@ fig, ax = plt.subplots()
 catch_data = pd.read_csv('fangstdata\elektronisk-rapportering-ers-2018-fangstmelding-dca.csv',delimiter=';', on_bad_lines='skip', encoding='utf8')
 #print(catch_data.columns)
 
-countries = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+#countries = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 herring_catches = catch_data.loc[catch_data['Art - FDIR'] == 'Sild']
 #herring_catches.loc['Startposisjon bredde'] = pd.to_numeric([pos.replace(',','.') for pos in herring_catches['Startposisjon bredde']])
 #herring_catches.loc['Startposisjon lengde'] = pd.to_numeric([pos.replace(',','.') for pos in herring_catches['Startposisjon lengde']])
 longitude = pd.to_numeric([pos.replace(',','.') for pos in herring_catches['Startposisjon bredde']])
 latitude = pd.to_numeric([pos.replace(',','.') for pos in herring_catches['Startposisjon lengde']])
+Rundvekt = pd.to_numeric([vekt for vekt in herring_catches['Rundvekt']])
 
 #herring_catches[:1000].plot.scatter(x='Startposisjon lengde', y='Startposisjon bredde', xlim=[0,90], ylim = [0,90], ax=ax)
 #global plot
@@ -42,8 +43,11 @@ def update(val):
 
 #for i in range(1,13):
 #    update(i)
-
+print(Rundvekt)
 #month_slider.on_changed(update)
+for i in range(len(Rundvekt)):
+    if Rundvekt[i] == 0:
+        Rundvekt[i] = 1
 plotter = {'latitude' : latitude, 'longitude': longitude}
 
 fig = px.density_mapbox(plotter, lat='longitude', lon='latitude',mapbox_style='stamen-terrain', opacity=.7, radius=5, title="Sildefangst 2018")
